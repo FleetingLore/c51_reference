1bcdf7d14a2e4c0c1d8e9d63059842989e05e21e
石山灰 <Macro__9@outlook.com>
2025-11-25 12:27:50 +0800

    add src 1

---
+++ project/src/1/delay.c
@@ -0,0 +1,20 @@
+/// 无符号整数类型的别名
+#define uchar unsigned char
+#define uint unsigned int
+
+/**
+ * [毫秒级延时函数]
+ * 
+ * 只是用循环来简单模拟，缺陷是对于不同的设备表现不一致。
+ */
+void delay_ms(
+    uint t /* 延时时间 */
+) 
+{
+    uchar j;  // 在函数开头定义变量
+    
+    while(t--) {
+        j = 85;
+        while (j--);
+    }
+}
---
+++ project/src/1/delay.h
@@ -0,0 +1,10 @@
+/// 无符号整数类型的别名
+#define uint unsigned int
+
+#ifndef __DELAY_H__
+#define __DELAY_H__
+
+/// 毫秒级延时函数
+void delay_ms(uint t);
+
+#endif
