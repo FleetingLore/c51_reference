4e75fb5aa763a7001970a64e90847cfb3383dd0d
石山灰 <Macro__9@outlook.com>
2025-11-29 10:50:06 +0800

    trim

--- project/src/1/lib/display/display.h
+++ project/src/1/lib/display/display.h
@@ -7,27 +7,20 @@
 #define __DISPLAY_H__
 
 /**
- * @brief 针对 K1 的按键检测
- * @return 按下 K1 则返回 1，否则返回 0
+ * @brief LED 模块以两个为一组从左向右移位，按一次移两位
  */
 int mode_1_join();
 
 /**
- * @brief 针对 K2 的按键检测
- * @return 按下 K2 则返回 2，否则返回 0
+ * @brief LED 模块从左向右亮，按照先奇后偶的顺序亮起
+ *        后灯亮前灯灭，一直循环，时间间隔 0.5 秒
+ *        再次按下 K2 停止，跳出循环。（1-3-5-7-2-4-6-8）
  */
 int mode_2_join();
 
 /**
- * @brief 针对 K3 的按键检测
- * @return 按下 K3 则返回 3，否则返回 0
+ * @brief 所有灯亮起，3 秒后全部熄灭
  */
 int mode_3_join();
-
-/**
- * @brief 针对 K4 的按键检测
- * @return 按下 K4 则返回 4，否则返回 0
- */
-int k4_on();
 
 #endif
